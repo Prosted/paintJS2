5b5b6b2310905ddfd86d73b3e2c59cd40c1da538
Prosted <69509019+Prosted@users.noreply.github.com>
2020-10-02 15:26:16 +0900

    add path and stroke
    선 그리기 기능 구현

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
-let painting=false;
+canvas.width = 700;
+canvas.height = 500;
 
-function stopPainting(evnet){
-    painting=false;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function startPainting(event) {
+  painting = true;
+}
+
+function stopPainting(evnet) {
+  painting = false;
 }
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-
-function onMouseDown(event){
-    painting=true;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseUp(event){
-    stopPainting();
+function onMouseDown(event) {
+  painting = true;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
