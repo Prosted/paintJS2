2592d44cf55bca6c384192ce105a77d4cb76783a
Prosted <69509019+Prosted@users.noreply.github.com>
2020-10-05 17:05:39 +0900

    add range change & fill mode
    range 조절에 따른 펜 굵기 조절기능과 채우기 기능 추가

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jscolor");
+const range = document.getElementById("range");
+const mode = document.getElementById("jsChange");
 
-canvas.width = 700;
-canvas.height = 500;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_WIDTH = "700";
+const CANVAS_HEIGHT = "500";
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function startPainting(event) {
   painting = true;
@@ -33,6 +41,28 @@ function onMouseMove(event) {
 function handleChangeColor(event) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handelRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handelModeChange(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "fill";
+  } else {
+    filling = true;
+    mode.innerText = "paint";
+  }
+}
+
+function handleCanvasClick(event) {
+  if (filling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
 }
 
 if (canvas) {
@@ -40,8 +70,17 @@ if (canvas) {
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 Array.from(colors).forEach((color) =>
   color.addEventListener("click", handleChangeColor)
 );
+
+if (range) {
+  range.addEventListener("input", handelRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handelModeChange);
+}
